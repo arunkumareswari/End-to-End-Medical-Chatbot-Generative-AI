5322e963cd188dda73e110d02f89828cd346597e
arunkumareswari <arkarunkumar1221@gmail.com>
2025-11-29 12:59:45 +0530

    feat: initialize project structure for HealthSync Chatbot

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'HealthSyncChatbot',
+    version = '0.0.0',
+    author = 'Arun Kumar',
+    author_email = 'aruneswari03@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+
+)
